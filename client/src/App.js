28772c23da7e372e6b7d4261e0645fc7177801f7
thristov <thristov@abrites.com>
2020-11-03 17:21:48 +0200

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 
+import { getPosts } from './actions/posts'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 
 import photo from './images/photo.png'
 import useStyles from './styles'
+
 const App = () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getPosts())
+        
+    }, [dispatch])
 
     return (
         <Container maxidth="lg">
