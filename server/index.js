a9705fbddbd4bfb390606411f08f02da0269381a
thristov <thristov@abrites.com>
2020-11-03 13:33:52 +0200

    database connected

--- server/index.js
+++ server/index.js
@@ -2,3 +2,23 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+import postRoutes from './routes/posts.js'
+
+const app = express()
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+//gonna secure it later
+const CONNECTION_URL = 'mongodb+srv://user:pass@softuni.etcdp.mongodb.net/around_the_world?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
