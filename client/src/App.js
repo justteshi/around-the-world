3a6a357925a18716fce9fa00fe6bdbcbffea3ed8
thristov <thristov@abrites.com>
2020-11-03 15:04:09 +0200

    client side structure and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+
+import photo from './images/photo.png'
+import useStyles from './styles'
 const App = () => {
+    const classes = useStyles()
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Travels</Typography>
+                <img className={classes.image} src={photo} alt='photo' height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing='{3}'>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -12,7 +12,7 @@ const postSchema = mongoose.Schema({
     },
     createdAt: {
         type: Date,
-        default: new Data()
+        default: new Date()
     },
 })
 
